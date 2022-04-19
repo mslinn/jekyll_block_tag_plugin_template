4ad0c2c5a753cfdd139fd54bee2bf4b37d825419
Mike Slinn <mslinn@micronauticsresearch.com>
2022-04-19 07:49:36 -0400

    -

--- lib/jekyll_plugin_template.rb
+++ lib/jekyll_plugin_template.rb
@@ -5,14 +5,14 @@ require "jekyll_plugin_logger"
 require_relative "jekyll_plugin_template/version"
 require_relative "jekyll_block_tag_plugin"
 require_relative "jekyll_tag_plugin"
-# require_relative "jekyll_hooks"
-# require_relative "jekyll_hook_examples"
+require_relative "jekyll_hooks"
+require_relative "jekyll_hook_examples"
 require_relative "category_index_generator"
 
 module JekyllPluginTemplate
   include JekyllBlockTagPlugin
   include JekyllTagPlugin
-  # include JekyllPluginHooks
-  # include JekyllHookExamples
+  include JekyllPluginHooks
+  include JekyllHookExamples
   include CategoryIndexGenerator
 end
